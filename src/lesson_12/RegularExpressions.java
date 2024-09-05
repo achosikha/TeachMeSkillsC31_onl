7ae98ac2337ba188ee3199ee831914f8b72a42a0
achosikha <softdevelopment@yahoo.com>
2024-09-05 16:00:51 +0400

    Strings advanced and regular expressions

---
+++ src/lesson_12/RegularExpressions.java
@@ -0,0 +1,13 @@
+package lesson_12;
+
+public class RegularExpressions {
+    public static void main(String[] args) {
+        // What are regular expressions?
+        // Why do we need them?
+
+        // Examples from our calculator?
+
+        // Class Pattern
+        // Class Matcher
+    }
+}
---
+++ src/lesson_12/StringsAdditional.java
@@ -0,0 +1,20 @@
+package lesson_12;
+
+public class StringsAdditional {
+    public static void main(String[] args) {
+        // toCharArray()
+        // trim()
+        // equalsIgnoreCase()
+        // toLowerCase()
+        // toUpperCase()
+        // valueOf()
+
+        // System.out
+        // printf()
+        // format()
+        // format style
+
+        // Class Formatter()
+        // String format()
+    }
+}
